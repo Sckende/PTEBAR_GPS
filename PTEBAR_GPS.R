8a1b5aadb88d864f0965ced0af9f5b8b75806f1d
unknown <Etudiant@5357320350.campus.univ.run>
2021-06-02 15:25:48 +0400

    # Speed summary by individuals # Trajectory plot by individuals

--- PTEBAR_GPS.R
+++ PTEBAR_GPS.R
@@ -1,17 +1,50 @@
-setwd("/home/claire/Bureau")
+setwd("C:/Users/Etudiant/Desktop/SMAC/Projet_publi/4-PTEBAR_GPS/DATA")
 gps <- read.csv2("PTEBAR_GPS_all.csv", dec = ".")
 gps
 head(gps)
 names(gps)
 summary(gps)
 table(gps$Logger_ID)
+table(gps$Year)
 
-gps$Logger_ID <- as.character(gps$Logger_ID)
+# Information summary about speed/bird
+logger_list <- split(gps, gps$Logger_ID)
+
 speed_bilan <- data.frame()
-for (i in unique(gps$Logger_ID)){
-  logger <- i
-  speed_mean <- mean(gps$Speed[gps$Logger_ID == i])
-  speed_max <- max(gps$Speed[gps$Logger_ID == i])
+for (i in 1:length(logger_list)){
+  logger <- unique(logger_list[[i]]$Logger_ID)
+  N_points <- length(logger_list[[i]]$Logger_ID)
   
-  speed_bilan <- rbind(speed_bilan, c(logger, speed_mean, speed_max))
+  speed_mean <- mean(logger_list[[i]]$Speed, na.rm = T)
+  speed_max <- max(logger_list[[i]]$Speed, na.rm = T)
+  NA_speed <- length(logger_list[[i]]$Speed[is.na(logger_list[[i]]$Speed)])
+  NA_prop_speed <- length(logger_list[[i]]$Speed[is.na(logger_list[[i]]$Speed)])/N_points*100
+  
+  
+  bilan <- data.frame(logger, N_points, speed_mean, speed_max, NA_speed, NA_prop_speed)
+  speed_bilan <- rbind(speed_bilan, bilan)
+}
+
+speed_bilan
+
+# GPS points plot
+
+head(gps)
+rainb_col <- rainbow(n = length(unique(gps$Logger_ID)))
+
+plot(x = logger_list[[1]]$Longitude[!is.na(logger_list[[1]]$Longitude)],
+     y = logger_list[[1]]$Latitude[!is.na(logger_list[[1]]$Latitude)],
+     type = "l",
+     col = rainb_col[1],
+     bty = 'n',
+     xlim = c(min(gps$Longitude, na.rm = T), max(gps$Longitude, na.rm = T)),
+     ylim = c(min(gps$Latitude, na.rm = T), max(gps$Latitude, na.rm = T)),
+     xlab = 'Longitude',
+     ylab = 'Latitude')
+
+for (i in 2: length(logger_list)){
+  lines(x = logger_list[[i]]$Longitude[!is.na(logger_list[[i]]$Longitude)],
+       y = logger_list[[i]]$Latitude[!is.na(logger_list[[i]]$Latitude)],
+       type = "l",
+       col = rainb_col[i])
 }
