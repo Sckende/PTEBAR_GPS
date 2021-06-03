dafbc6652c6cad73ef32bb572bfc74672c688161
unknown <Etudiant@5357320350.campus.univ.run>
2021-06-03 16:24:27 +0400

    # Start with moveVis package

--- PTEBAR_GPS.R
+++ PTEBAR_GPS.R
@@ -14,6 +14,8 @@ speed_bilan <- data.frame()
 for (i in 1:length(logger_list)){
   logger <- unique(logger_list[[i]]$Logger_ID)
   N_points <- length(logger_list[[i]]$Logger_ID)
+  ini_date <- paste0(c(logger_list[[i]]$Year[1], logger_list[[i]]$Month[1], logger_list[[i]]$Day[1]), collapse = "-")
+  end_date <- paste0(c(logger_list[[i]]$Year[N_points], logger_list[[i]]$Month[N_points], logger_list[[i]]$Day[N_points]), collapse = "-")
   
   speed_mean <- mean(logger_list[[i]]$Speed, na.rm = T)
   speed_max <- max(logger_list[[i]]$Speed, na.rm = T)
@@ -21,19 +23,25 @@ for (i in 1:length(logger_list)){
   NA_prop_speed <- length(logger_list[[i]]$Speed[is.na(logger_list[[i]]$Speed)])/N_points*100
   
   
-  bilan <- data.frame(logger, N_points, speed_mean, speed_max, NA_speed, NA_prop_speed)
+  bilan <- data.frame(logger, N_points, ini_date, end_date, speed_mean, speed_max, NA_speed, NA_prop_speed)
   speed_bilan <- rbind(speed_bilan, bilan)
 }
 
 speed_bilan
+# Individuals to remove
+# PAC04, PAC05, PAC13
+# => balises de 2019 non fonctionnelles ?
 
 # GPS points plot
 
 head(gps)
-rainb_col <- rainbow(n = length(unique(gps$Logger_ID)))
 
-plot(x = logger_list[[1]]$Longitude[!is.na(logger_list[[1]]$Longitude)],
-     y = logger_list[[1]]$Latitude[!is.na(logger_list[[1]]$Latitude)],
+gps2 <- gps[!(gps$Logger_ID %in% c("PAC04", "PAC05", "PAC13")),]
+rainb_col <- rainbow(n = length(unique(gps2$Logger_ID)))
+logger_list2 <- split(gps2, gps2$Logger_ID)
+
+plot(x = logger_list2[[1]]$Longitude[!is.na(logger_list2[[1]]$Longitude)],
+     y = logger_list2[[1]]$Latitude[!is.na(logger_list2[[1]]$Latitude)],
      type = "l",
      col = rainb_col[1],
      bty = 'n',
@@ -42,9 +50,51 @@ plot(x = logger_list[[1]]$Longitude[!is.na(logger_list[[1]]$Longitude)],
      xlab = 'Longitude',
      ylab = 'Latitude')
 
-for (i in 2: length(logger_list)){
-  lines(x = logger_list[[i]]$Longitude[!is.na(logger_list[[i]]$Longitude)],
-       y = logger_list[[i]]$Latitude[!is.na(logger_list[[i]]$Latitude)],
+for (i in 2: length(logger_list2)){
+  lines(x = logger_list2[[i]]$Longitude[!is.na(logger_list2[[i]]$Longitude)],
+       y = logger_list2[[i]]$Latitude[!is.na(logger_list2[[i]]$Latitude)],
        type = "l",
        col = rainb_col[i])
 }
+
+#### moveVis package ####
+library(moveVis)
+library(move)
+library(raster)
+library(ggplot2)
+
+# Package example
+# http://movevis.org/articles/example-1.html
+data("move_data")
+
+unique(timestamps(move_data))
+timeLag(move_data, unit = "mins")
+
+move_data <- align_move(move_data, res = 4, unit = "mins")
+
+frames <- frames_spatial(move_data, path_colours = c("red", "green", "blue"),
+                         map_service = "osm", map_type = "watercolor", alpha = 0.5)
+
+length(frames) # number of frames
+frames[[100]] # display one of the frames
+frames[[130]]
+
+animate_frames(frames, out_file = "example_1.gif")
+
+
+# Conversion of dataframe to move object
+
+#time has to be of class POSIXct
+names(gps2)
+gps2$timest <- paste(paste(gps2$Year, gps2$Month, gps2$Day, sep = "-"), paste(gps2$Hour, gps2$Minute, gps2$Second, sep = ":"), sep = " ")
+
+gps2$timest <- as.POSIXct(gps2$timest)
+
+gps3 <- gps2[!is.na(gps2$Latitude),]
+gps3 <- gps3[gps3$Logger_ID == "PAC15",]
+gps3_move <- df2move(gps3,
+                     proj = "+init=epsg:4326 +proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0",
+                     x = "Longitude",
+                     y = "Latitude",
+                     time = "timest",
+                     track_id = "Logger_ID")
