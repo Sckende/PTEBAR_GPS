3c22a113ffe01cbbcaadb7ac8e7276df0bb1736c
unknown <Etudiant@5357320350.campus.univ.run>
2021-06-22 14:13:59 +0400

    # Data cleaning & exploration

--- PTEBAR_TRIP_pck_post-process_gps_data.R
+++ PTEBAR_TRIP_pck_post-process_gps_data.R
@@ -1,23 +1,24 @@
 # Use of TRIP package for the post-process of the gps data
 rm(list = ls())
 
-require(trip)
 gps <- read.csv2("C:/Users/Etudiant/Desktop/SMAC/Projet_publi/4-PTEBAR_GPS/DATA/PTEBAR_GPS_all.csv", dec = ".")
 summary(gps)
 names(gps)
 
-# Creation of 'time' variable
+#### Creation of 'time' variable ####
 gps$time <- paste(paste(gps$Year, gps$Month, gps$Day, sep = "-"), paste(gps$Hour, gps$Minute, gps$Second, sep = ":"), sep = " ")
 gps$time <- as.POSIXct(gps$time)
 
-# here keeping only date, ID, lat & long
-gps1 <- gps[, c(2, 9, 10, 29)]
+names(gps)
+#### here keeping only date, ID, lat & long, speed, searching_time, Voltage ####
+gps1 <- gps[, c(2, 9:13, 29)]
 
-# For each logger, % of missing data
+#### For each logger, % of missing data, max/min speed, ... ####
 #all(is.na(gps$Latitude) == is.na(gps$Longitude)) # check point
 
 gps_list <- split(gps1, gps1$Logger_ID)
 
+# NA summary
 bilan <- data.frame()
 
 for (i in 1:length(gps_list)){
@@ -32,19 +33,58 @@ for (i in 1:length(gps_list)){
 names(bilan) <- c('log_ID', 'point_numb', 'NA_numb', 'prop_NA')
 bilan[order(as.numeric(bilan$point_numb)),]
 
-# Visual explo
+# Delete the duplicated rows for DATE/TIME based on the lower searching_time
+bilan2 <- data.frame()
+
+for(i in 1:length(gps_list)){
+  t <- gps_list[[i]][!is.na(gps_list[[i]]$Latitude),]
+  t <- t[order(t$time, t$Searching_time, decreasing = F),]
+  t <- t[!duplicated(t$time),]
+  
+  log_ID <- unique(t$Logger_ID)
+  point_numb <- nrow(t)
+  time_min <- as.character(min(t$time))
+  time_max <- as.character(max(t$time))
+  speed_min <- min(t$Speed)
+  speed_max <- max(t$Speed)
+  
+  bilan2 <- rbind(bilan2, c(log_ID, point_numb, time_min, time_max, speed_min, speed_max))
+}
+names(bilan2) <- c('log_ID', 'point_numb', 'time_min', 'time_max', 'speed_min', 'speed_max')
+bilan2[order(as.numeric(bilan2$point_numb)),]
+
+#### DELETION of PAC04, PAC13 & PAC05 ####
+# Due to low number of GPS fixes
+k <- c('PAC04', 'PAC13', 'PAC05')
+no <- setdiff(names(gps_list), k)
+gps_list2 <- gps_list[no] # keeping list levels with data of interest
+
+#### Visual explo ####
+#require(trip)
 require(mapview)
 
-for (i in 1:length(gps_list)){
-  g <- gps_list[[i]][!is.na(gps_list[[i]]$Latitude),]
-  sp::coordinates(g) <- c('Longitude', 'Latitude')
-  #g_sf <- sf::st_as_sf(g)
-  #g_sf <- sf::st_cast(g_sf, 'LINESTRING')
-  #mapview::mapview(g)
-  plot(g)
-  lines(g)
+# data conversion in SF LINESTRING
+track_lines <- data.frame()
+track_list <- list()
+
+par(mfrow = c(2,4))
+projcrs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+
+for(i in 1:length(gps_list2)){
+  tk <- gps_list2[[i]]
+  
+  tk <- tk[!is.na(tk$Latitude),]
+  tk <- tk[order(tk$time, decreasing = F),]
+  coord <- as.matrix(tk[, c('Longitude', 'Latitude')])
+  track <- sf::st_linestring(coord)
+  
+  track_lines <- rbind(track_lines, c(unique(tk$Logger_ID), sf::st_linestring(coord)))
+  track_list[[i]] <- track
+  names(track_list)[i] <- unique(tk$Logger_ID)
+  plot(track)
 }
 
-sp::coordinates(gps_list[[1]]) <- c("Longitude", "Latitude")
-gps_sf <- sf::st_as_sf(gps_list[[1]])
-mapview::mapview(gps_sf, zcol = 'tripID') # Wonderful map to develop !!!!
+mapview::mapview(track_list,
+                 color = rainbow(n = length(track_list)),
+                 legend = T)
+
